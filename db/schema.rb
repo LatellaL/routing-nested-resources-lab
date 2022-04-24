97055099b647e4912f6c9e22d81058299c70d6b7
LatellaL <83371530+LatellaL@users.noreply.github.com>
2022-04-23 21:09:06 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -1,7 +1,16 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
+
+    
+        
+          
+    
+
+        
+    
+@@ -24,8 +23,7 @@
+  
 #
 # Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
@@ -10,22 +19,18 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 20160126235547) do
-
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
   create_table "songs", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
-
-  add_index "songs", ["artist_id"], name: "index_songs_on_artist_id"
 
 end
